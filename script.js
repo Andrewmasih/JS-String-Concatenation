33723d01bd12bfd63aace136186b94ce56d95c9b
Andrew Masih <armasih@outlook.com>
2021-05-18 23:28:43 +0000

    added styling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let firstString = "I am";
 let secondString = " in love with JavaScript";
+
 let newString = firstString + secondString;
-console.log (newString);
+
+console.log(newString);
 
 /* string concatenation is joining character strings end to end. it joins 2 or more strings, use the '+' operator, the 'concat' method or 'template literals' to jion 2or more strings. */
